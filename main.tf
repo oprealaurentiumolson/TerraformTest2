bb5b232e97578578b477cca9b5bc24baec3bc940
oprealaurentiumolson <mihailaurentiu.oprea@molsoncoors.com>
2025-02-11 16:51:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,19 +14,8 @@ source = "github.com/somleng/terraform-aws-cloudfront-reverse-proxy"
   # Somewhat perplexingly, this is the "correct" way to ensure users can't bypass CloudFront on their way to S3 resources
   # https://abridge2devnull.com/posts/2018/01/restricting-access-to-a-cloudfront-s3-website-origin/
   origin_custom_header_value = "${random_string.s3_read_password.result}"
-
-  site_domain            = "${var.site_domain}"
-  name_prefix            = "${var.name_prefix}"
-  comment_prefix         = "${var.comment_prefix}"
-  cloudfront_price_class = "${var.cloudfront_price_class}"
-  viewer_https_only      = "${var.viewer_https_only}"
-  cache_ttl_override     = "${var.cache_ttl_override}"
-  default_root_object    = "${var.default_root_object}"
-  add_response_headers   = "${var.add_response_headers}"
-  basic_auth_username    = "${var.basic_auth_username}"
-  basic_auth_password    = "${var.basic_auth_password}"
-  basic_auth_realm       = "${var.basic_auth_realm}"
-  basic_auth_body        = "${var.basic_auth_body}"
-  lambda_logging_enabled = "${var.lambda_logging_enabled}"
-  tags                   = "${var.tags}"
+host = "your-domain.example.com"
+  origin = "your-domain.app.somleng.org"
+  zone_id = aws_route53_zone.example_com.zone_id # Optional. Leave blank if not using route53.
+  certificate_arn = "existing-certificate-arn" # Optional. Leave blank to create a new certificate.
 }
